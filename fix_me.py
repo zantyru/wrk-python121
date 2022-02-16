37d011566fbab9d24d112a93157ee6d76b0607b6
Barinova Wera <barinova_v@ru.itstep.org>
2022-02-16 20:03:03 +0300

    Initial commit.

---
+++ fix_me.py
@@ -0,0 +1,80 @@
+class WaterDrop:
+    def __init__(self, volume=1.0):
+        self.volume = float(volume)
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return f"{class_name}({self.volume})"
+
+    def _check_type(self, other):
+        if not isinstance(other, WaterDrop):
+            raise TypeError("Ожидается экземпляр класса WaterDrop, "
+                            f"но получен тип {type(other)}.")
+
+    def __add__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume + other.volume)
+        return result
+
+    def __sub__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume - other.volume)
+        return result
+
+    def __mul__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume * other.volume)
+        return result
+
+    def __truediv__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume / other.volume)
+        return result
+
+    def __eq__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume == other.volume)
+        return result
+
+    def __ne__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume != other.volume)
+        return result
+
+    def __lt__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume < other.volume)
+        return result
+
+    def __le__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume <= other.volume)
+        return result
+
+    def __gt__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume > other.volume)
+        return result
+
+    def __ge__(self, other):
+        self._check_type(other)
+        result = WaterDrop(self.volume >= other.volume)
+        return result
+
+if __name__ == '__main__':
+    wd1 = WaterDrop(10)
+    wd2 = WaterDrop()
+
+    wd3 = wd1 + wd2  # wd1.__add__(wd2)  # WaterDrop.__add__(wd1, wd2)
+    wd4 = wd2 + wd1
+
+    print(wd3, id(wd3))
+    print(wd4, id(wd4))
+
+    print("----------------")
+
+    print(wd2, id(wd2))
+    wd2 += wd2  # wd2 = wd2 + wd2
+    print(wd2, id(wd2))
+    wd2 += wd2  # wd2 = wd2 + wd2
+    print(wd2, id(wd2))
